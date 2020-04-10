937c9a5bbc72d180f61b0d8e39a8eb2629b27221
Tom Szelag <szelag@gmail.com>
2020-04-10 08:34:55 -0400

    Add AlgLib2 package and some basic Gauss function implementation

---
+++ PreProcessor/Gauss.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreProcessor
+{
+    internal static class Gauss
+    {
+        internal static double Evaluate(double x, double a, double b, double c)
+        {
+            var v1 = (x - b);
+            var v2 = (v1 * v1) / (2 * (c * c));
+            var v3 = a * Math.Exp(-v2);
+            return v3;
+        }
+
+        internal static void EvaluateWrapper(double[] c, double[] x, ref double y, object obj)
+        {
+            y = Evaluate(x[0], c[0], c[1], c[2]);
+        }
+    }
+}
